5c44277894e8c3c637d27d4be54bb5dac562b0d6
Bruce Lee <jangsoo.lee@gmail.com>
2016-03-07 15:05:27 -0500

    more sensible default

--- app/lib/Utility.scala
+++ app/lib/Utility.scala
@@ -2,6 +2,6 @@ package lib
 
 object Utility {
   def isLogOnly() = {
-    scala.util.Properties.envOrElse("GUWHO_LOGONLY", "false").toBoolean
+    scala.util.Properties.envOrElse("GUWHO_LOGONLY", "true").toBoolean
   }
 }
